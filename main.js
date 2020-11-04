1599d39e09d6021d265780f77c489e71368e2138
paulinemss <masse.pauline@gmail.com>
2020-11-04 17:51:27 +0100

    Touch up main js file

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
 function preload() {
   // loading spritesheets
-  playerIdle = loadSpriteSheet("/assets/player/idle.png", 32, 32, 11);
-  playerIdleLeft = loadSpriteSheet("/assets/player/idleLeft.png", 32, 32, 11);
-  playerRunRight = loadSpriteSheet("/assets/player/runRight.png", 32, 32, 12);
-  playerRunLeft = loadSpriteSheet("/assets/player/runLeft.png", 32, 32, 12);
-  playerJump = loadSpriteSheet("/assets/player/jump.png", 32, 32, 1);
-  playerDblJump = loadSpriteSheet("/assets/player/doubleJump.png", 32, 32, 6);
-  monsterRunRight = loadSpriteSheet("/assets/monster/runRight.png", 44, 30, 10);
-  monsterRunLeft = loadSpriteSheet("/assets/monster/runLeft.png", 44, 30, 10);
-  endPoint = loadSpriteSheet("/assets/terrain/endPoint.png", 64, 64, 17);
-  torchOn = loadSpriteSheet("/assets/terrain/torchOn.png", 16, 32, 3);
-  torchOff = loadSpriteSheet("/assets/terrain/torchOff.png", 16, 32, 1);
-  playerFall = loadSpriteSheet("/assets/player/fall.png", 32, 32, 1);
-  transformRunLeft = loadSpriteSheet("/assets/monster/newMonster/radishRunLeft.png", 30, 38, 12); 
-  transformRunRight = loadSpriteSheet("/assets/monster/newMonster/radishRunRight.png", 30, 38, 12);
+  playerIdle = loadSpriteSheet("./assets/player/idle.png", 32, 32, 11);
+  playerIdleLeft = loadSpriteSheet("./assets/player/idleLeft.png", 32, 32, 11);
+  playerRunRight = loadSpriteSheet("./assets/player/runRight.png", 32, 32, 12);
+  playerRunLeft = loadSpriteSheet("./assets/player/runLeft.png", 32, 32, 12);
+  playerJump = loadSpriteSheet("./assets/player/jump.png", 32, 32, 1);
+  playerDblJump = loadSpriteSheet("./assets/player/doubleJump.png", 32, 32, 6);
+  monsterRunRight = loadSpriteSheet("./assets/monster/runRight.png", 44, 30, 10);
+  monsterRunLeft = loadSpriteSheet("./assets/monster/runLeft.png", 44, 30, 10);
+  endPoint = loadSpriteSheet("./assets/terrain/endPoint.png", 64, 64, 17);
+  torchOn = loadSpriteSheet("./assets/terrain/torchOn.png", 16, 32, 3);
+  torchOff = loadSpriteSheet("./assets/terrain/torchOff.png", 16, 32, 1);
+  playerFall = loadSpriteSheet("./assets/player/fall.png", 32, 32, 1);
+  transformRunLeft = loadSpriteSheet("./assets/monster/newMonster/radishRunLeft.png", 30, 38, 12); 
+  transformRunRight = loadSpriteSheet("./assets/monster/newMonster/radishRunRight.png", 30, 38, 12);
 
   // loading images, fonts 
-  bgGeneral = loadImage("/assets/terrain/Background-dark.png");
-  bgIntro = loadImage("/assets/terrain/Intro.png");
-  bgLevelOne = loadImage("/assets/terrain/Level1.png");
-  bgLevelOneDark1 = loadImage("/assets/terrain/Dark-Level1-1.png");
-  bgLevelOneDark2 = loadImage("/assets/terrain/Dark-Level1-2.png");
-  bgLevelTwo = loadImage("/assets/terrain/Level2.png");
-  bgLevelThree = loadImage("/assets/terrain/Level3.png");
-  bgConclusion = loadImage("/assets/terrain/Conclusion.png");
-  platform = loadImage("/assets/terrain/platform.png");
-  spikeBall = loadImage("/assets/terrain/spikeBall.png");
-  gameFont = loadFont("/assets/font/PressStart2P-Regular.ttf")
+  bgGeneral = loadImage("./assets/terrain/Background-dark.png");
+  bgIntro = loadImage("./assets/terrain/Intro.png");
+  bgLevelOne = loadImage("./assets/terrain/Level1.png");
+  bgLevelOneDark1 = loadImage("./assets/terrain/Dark-Level1-1.png");
+  bgLevelOneDark2 = loadImage("./assets/terrain/Dark-Level1-2.png");
+  bgLevelTwo = loadImage("./assets/terrain/Level2.png");
+  bgLevelThree = loadImage("./assets/terrain/Level3.png");
+  bgConclusion = loadImage("./assets/terrain/Conclusion.png");
+  platform = loadImage("./assets/terrain/platform.png");
+  spikeBall = loadImage("./assets/terrain/spikeBall.png");
+  gameFont = loadFont("./assets/font/PressStart2P-Regular.ttf")
 
   // loading animations 
   playerIdleAnimation = loadAnimation(playerIdle);
